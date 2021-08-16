e1e16286dab17cf5b5f19c8c863e036dd143add7
ngweijian1 <70877795+ngweijian1@users.noreply.github.com>
2021-08-16 21:35:26 +0800

    Update qrScan.dart

--- lib/pages/qrScan.dart
+++ lib/pages/qrScan.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mysj/widgets/checkin.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 
 import 'dart:io' show Platform;
 
@@ -75,20 +75,31 @@ class _ScanPageState extends State<ScanPage> {
 
   void onQRViewCreated(QRViewController controller) async {
     setState(() => this.controller = controller);
-    controller.scannedDataStream
-        .listen((barcode) async{
-          this.barcode = barcode;
-          
-    var now = DateTime.now();
-    final DateFormat formatter = DateFormat('yyyy-MM-dd');
-    final String dateNow = formatter.format(now);
-    userID = FirebaseAuth.instance.currentUser.uid;
-    final CollectionReference usersCollection = FirebaseFirestore.instance.collection("users");
-    final String timeNow = (now.hour.toString() + ":" + now.minute.toString() + ":" + now.second.toString());
+    controller.scannedDataStream.listen((barcode) async {
+      await controller.pauseCamera();
+      this.barcode = barcode;
 
-    await usersCollection.doc(userID).collection('travel history').add({"place": '${barcode.code}', "time": timeNow, "date": dateNow});
-    }
-    );
+      var now = DateTime.now();
+      final DateFormat formatter = DateFormat('yyyy-MM-dd');
+      final String dateNow = formatter.format(now);
+      userID = FirebaseAuth.instance.currentUser.uid;
+      final CollectionReference usersCollection =
+          FirebaseFirestore.instance.collection("users");
+      final String timeNow = (now.hour.toString() +
+          ":" +
+          now.minute.toString() +
+          ":" +
+          now.second.toString());
+
+      await usersCollection
+          .doc(userID)
+          .collection('travel history')
+          .add({"place": '${barcode.code}', "time": timeNow, "date": dateNow});
+      Navigator.pop(context);
+
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => CheckIn()));
+    });
   }
-        
+  
 }
