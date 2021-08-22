0cfd36e858b95959d0084a9ec1db45f68546fdad
chia7123 <84016807+chia7123@users.noreply.github.com>
2021-08-23 00:47:41 +0800

    update

--- lib/authentication/wrapper.dart
+++ lib/authentication/wrapper.dart
@@ -47,3 +47,4 @@ class Wrapper extends StatelessWidget {
     );
   }
 }
+
--- lib/pages/admin.dart
+++ lib/pages/admin.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:myselamat/authentication/welcome.dart';
 import 'package:myselamat/authentication/wrapper.dart';
 import 'package:myselamat/widgets/admin_quick_actions.dart';
 
